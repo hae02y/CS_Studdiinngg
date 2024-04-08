a1d82f2ebd81e0b2a02d181771b17a15e6adc25c
luminousol <luminousolding@gmail.com>
2024-04-08 18:09:06 +0900

    feat : singleton 추가

---
+++ 면접을 위한 CS 전공지식 노트/soli/ch1_designpattern_programmingparadigm/ch1/Singleton.java
@@ -0,0 +1,22 @@
+public class Singleton {
+    private static class singleInstanceHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return singleInstanceHolder.INSTANCE;
+    }
+
+    public static class HelloWorld {
+        public static void main(String[] args) {
+            Singleton a = Singleton.getInstance();
+            Singleton b = Singleton.getInstance();
+            System.out.println(a.hashCode());
+            System.out.println(b.hashCode());
+
+            if(a==b) {
+                System.out.println(true);
+            }
+        }
+    }
+}
